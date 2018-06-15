1c311ba7139e7aef3207d6faf7bea648973d128c
Lee Lup Yuen <lupyuen@gmail.com>
2018-06-15 22:44:41 +0800

    Undo OBJ_DIR

--- rust.mk
+++ rust.mk
@@ -8,6 +8,5 @@ application:
 	cd src/rust && rustup override set nightly
 	cd src/rust && xargo build --target thumbv7m-none-eabi -v
 	mkdir -p $(OBJ_DIR)
-	rm $(OBJ_DIR)/*
 	cp src/rust/target/thumbv7m-none-eabi/debug/librustl8710.rlib $(OBJ_DIR)/librustl8710.o
 	cp ~/.xargo/lib/rustlib/thumbv7m-none-eabi/lib/libcore*.rlib $(OBJ_DIR)/libcore.o
