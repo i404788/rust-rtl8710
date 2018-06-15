a620ab151a646891507c358ba9210121459230c6
Lee Lup Yuen <lupyuen@gmail.com>
2018-06-15 22:38:14 +0800

    Copy libcore

--- rust.mk
+++ rust.mk
@@ -9,3 +9,4 @@ application:
 	cd src/rust && xargo build --target thumbv7m-none-eabi -v
 	mkdir -p $(OBJ_DIR)
 	cp src/rust/target/thumbv7m-none-eabi/debug/librustl8710.rlib $(OBJ_DIR)/librustl8710.o
+	cp ~/.xargo/lib/rustlib/thumbv7m-none-eabi/lib/libcore*.rlib $(OBJ_DIR)/libcore.o
