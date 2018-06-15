df274ecc753c514cd6eed59a738bcf0a58a6980d
Lee Lup Yuen <lupyuen@gmail.com>
2018-06-15 19:23:56 +0800

    Build xargo in verbose mode

--- rust.mk
+++ rust.mk
@@ -6,6 +6,6 @@ OBJ_DIR=$(TARGET)/Debug/rust_obj
 .PHONY: application
 application:
 	cd src/rust && rustup override set nightly
-	cd src/rust && xargo build --target thumbv7m-none-eabi
+	cd src/rust && xargo build --target thumbv7m-none-eabi -v
 	mkdir -p $(OBJ_DIR)
 	cp src/rust/target/thumbv7m-none-eabi/debug/librustl8710.rlib $(OBJ_DIR)/librustl8710.o
